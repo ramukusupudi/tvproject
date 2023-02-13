7b7dd532a9103640583bd6cbc3c6a8e55c51af8e
AshaBiju <101193143+AshaBiju@users.noreply.github.com>
2023-02-13 12:51:54 -0800

    Updated Employee's Verification script

--- DataMigrationQA/Employees_Model/employee_verification_script.sql
+++ DataMigrationQA/Employees_Model/employee_verification_script.sql
@@ -1,4 +1,4 @@
---Employee's Verification Script
+--Employee's Verification Script----
 
 INSERT INTO source_target_match (source_datasetId, source_id, source_field, source_value, expected_mapped_value, target_id, target_field, target_value, matched, notes)
 SELECT 
