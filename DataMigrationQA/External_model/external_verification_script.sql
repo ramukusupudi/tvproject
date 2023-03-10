55d8b5c1668db844d661295cb5d61d0d5bb6ff8a
Ramu Kusupudi <ramu.kusupudi@globallogic.com>
2023-03-10 15:19:26 +0000

    Update file external_verification_script.sql

--- DataMigrationQA/External_model/external_verification_script.sql
+++ DataMigrationQA/External_model/external_verification_script.sql
@@ -1,5 +1,5 @@
 --DELETE FROM source_target_match WHERE  source_datasetId = 'external';
-select * FROM source_target_match WHERE  source_datasetId = 'external' and matched ='FALSE' and target_value is not null
+--select * FROM source_target_match WHERE  source_datasetId = 'external' and matched ='FALSE' and target_value is not null
 INSERT INTO source_target_match (source_datasetId, source_id, source_field, source_value, expected_mapped_value, target_id, target_field, target_value, matched, notes)
 SELECT 
   'external' as source_datasetId,
