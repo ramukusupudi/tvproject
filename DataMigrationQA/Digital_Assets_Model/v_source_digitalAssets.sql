08b2396987b9ab8cf674c0bceea337ff37a4ebac
AshaBiju <101193143+AshaBiju@users.noreply.github.com>
2023-03-10 11:30:01 -0800

    Deleted folders created by Asha

--- DataMigrationQA/Digital_Assets_Model/v_source_digitalAssets.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW public.v_source_digitalAssets
- AS
- select uid,patient,insurance,front,back from insurance_cards ic;
- 
- ALTER TABLE public.v_source_digitalAssets
-    OWNER TO postgres;
-	
-	
-	
